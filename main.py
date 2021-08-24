95b460c091ece688d3f5c8e8a8fe8227b9cf6b2d
Wiko Laygunata <88517860+WikoLaygunata@users.noreply.github.com>
2021-08-24 13:23:51 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+print("penentu keliling dan luas segitiga\n")
+alas = float(input("Masukkan panjang alas segitiga = "))
+tinggi = float(input("Masukkan tinggi segitiga =  "))
+print(f"\nalas adalah {alas}cm dan tinggi adalah {tinggi}cm")
+
+Keliling = 3*alas
+Luas = alas*tinggi/2
+print(f"Keliling adalah {Keliling}cm")
+print(f"Luas adalah {Luas}cm")
