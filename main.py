06dd41cf487d024b85beece5ef33a5420181a688
Wiko Laygunata <88517860+WikoLaygunata@users.noreply.github.com>
2021-08-24 13:24:17 +0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-print("penentu keliling dan luas segitiga\n")
-alas = float(input("Masukkan panjang alas segitiga = "))
-tinggi = float(input("Masukkan tinggi segitiga =  "))
-print(f"\nalas adalah {alas}cm dan tinggi adalah {tinggi}cm")
-
-Keliling = 3*alas
-Luas = alas*tinggi/2
-print(f"Keliling adalah {Keliling}cm")
-print(f"Luas adalah {Luas}cm")
